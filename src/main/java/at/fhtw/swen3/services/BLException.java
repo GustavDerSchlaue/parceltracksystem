2bfa540715cb8acfb5a3425a020a9f1de77e5005
Leon von Voss <if20b169@technikum-wien.at>
2023-01-09 18:02:56 +0100

    Update BLException.java

--- src/main/java/at/fhtw/swen3/services/BLException.java
+++ src/main/java/at/fhtw/swen3/services/BLException.java
@@ -1,4 +1,22 @@
 package at.fhtw.swen3.services;
 
-public class BLException {
+import at.fhtw.swen3.persistence.entities.ErrorEntity;
+import lombok.Getter;
+
+@Getter
+public class BLException extends Exception {
+
+    private Exception innerException;
+    private ErrorEntity errorEntity;
+
+    public BLException(long errorId, String errorMsg, Exception e) {
+        this.innerException = e;
+        this.errorEntity = new ErrorEntity();
+        errorEntity.setId(errorId);
+        errorEntity.setErrorMessage(errorMsg);
+    }
+
+    public String getMessage() {
+        return errorEntity.getErrorMessage();
+    }
 }
